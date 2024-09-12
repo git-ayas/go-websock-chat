df9af9b70744c39cd7c0d31ad88e601c40907bce
git-ayas <32291808+git-ayas@users.noreply.github.com>
2024-09-12 14:38:44 +0530

    Observable (#1)
    * scaffolding and basic broker loop
    
    * pub sub methods added
    
    * Added test
    
    * [Dirty]  Implemented the observvable
    
    * refactoring
    
    * working live messaging
    
    * Housekeeping
    
    * Super helpful log changes

--- observable.go
+++ observable.go
@@ -1,19 +1,134 @@
 package main
 
-import "github.com/gofiber/contrib/websocket"
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
 
+	"github.com/gofiber/contrib/websocket"
+)
+
+type ObserverEntry struct {
+	id         string
+	connection *websocket.Conn
+}
+
+type MessageData struct {
+	sender  string
+	message string
+}
 type MessageObservable struct {
-	observers []*websocket.Conn
+	observers          []ObserverEntry
+	MessagesChannel    chan MessageData
+	UnsubscribeChannel chan ObserverEntry
+	SubscriberChannel  chan ObserverEntry
+	terminateObs       chan int
+	logger             *log.Logger
+	mu                 sync.Mutex
 }
 
-func (m *MessageObservable) Consume() {
+/*
+Its suppoosed to spell broker,but I like the typo
+more than being accurate. So we now have a friendly
+Borker who is fetching us messages.
+*/
+func (m *MessageObservable) Borker() {
+	observerCount := len(m.observers)
+	ticker := time.Tick(8 * time.Second)
+	allowDebouncedStatusLog := true
+	for {
+		select {
+		case message := <-m.MessagesChannel:
+			processedOut, err := GetParsedMessageComponent(MesageDataType{
+				Sender:  message.sender,
+				Content: message.message,
+			})
+			if err != nil {
+				m.logger.Println("Error parsing temp for broadcast.")
+			}
+			m.mu.Lock()
+			// Write out messages to all observers
+			for _, observer := range m.observers {
+				observer.connection.WriteMessage(1, processedOut)
+				m.logger.Printf("Woof! Sending data(%v) to %s  ", message, observer.id)
+			}
+			m.mu.Unlock()
+		case <-m.terminateObs:
+			fmt.Println("Terminating messaging observable")
+			return
+		case subscriber := <-m.SubscriberChannel:
+			m.mu.Lock()
+			m.observers = append(m.observers, subscriber)
+			m.logger.Printf("New subscriber connected 🐕<Perking Ear Floofs>")
+			m.mu.Unlock()
+		case subscriber := <-m.UnsubscribeChannel:
+			m.mu.Lock()
+			for i, observer := range m.observers {
+				if observer.id == subscriber.id {
+					m.observers = append(m.observers[:i], m.observers[i+1:]...)
+					m.logger.Printf("Subscriber left 🐕<sad borker noises>")
+				}
+			}
+			m.mu.Unlock()
+		case <-ticker:
+			allowDebouncedStatusLog = true
+
+		default:
+			if allowDebouncedStatusLog {
+				m.mu.Lock()
+				if len(m.observers) > observerCount {
+					m.logger.Printf("Woof! %d new subscriber/s 🐕<chases tail wildly> \n", len(m.observers)-observerCount)
+					observerCount = len(m.observers)
+				}
+				if observerCount < 1 {
+					m.logger.Printf("No subscribers connected. 🐕<borker lonely :( > \n")
+
+				} else {
+					m.logger.Printf("%d subscribers connected. 🐕<tail wags>", observerCount)
+				}
+				allowDebouncedStatusLog = false
+				m.mu.Unlock()
+			}
+		}
+	}
 
 }
 
-func (m *MessageObservable) Produce() {
+/*
+Some of the below are not used in this example, but it is a good example
+of how to use the observer pattern and are meant to be usage examples
+for the channels as well.
+
+They can be used to parse inputs before processing.
+It is also deferable allowing you to use it in a cleanup cycle.
+*/
+func (m *MessageObservable) Produce(message MessageData) {
+	m.MessagesChannel <- message
 
 }
 
-func (m *MessageObservable) Subscribe() {
+func (m *MessageObservable) Subscribe(subscriber ObserverEntry) {
+	m.logger.Printf("Subscribing %s", subscriber.id)
+	m.SubscriberChannel <- subscriber
 
+}
+func (m *MessageObservable) Unsubscribe(subscriber ObserverEntry) {
+
+	m.UnsubscribeChannel <- subscriber
+
+}
+func (m *MessageObservable) Terminate() {
+	m.terminateObs <- 1
+}
+
+func NewMessageObservable(logger *log.Logger) *MessageObservable {
+	return &MessageObservable{
+		observers:          []ObserverEntry{},
+		MessagesChannel:    make(chan MessageData),
+		UnsubscribeChannel: make(chan ObserverEntry),
+		SubscriberChannel:  make(chan ObserverEntry),
+		terminateObs:       make(chan int),
+		logger:             logger,
+	}
 }
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/gofiber/contrib/websocket"
@@ -12,10 +13,22 @@ import (
 	"github.com/goombaio/namegenerator"
 )
 
-var host, port = flag.String("host", "192.168.1.14", "Name of host"), flag.Int("port", 3174, "Port to listen on")
+var host, port = flag.String("host", "127.0.0.1", "Host name to listen on"), flag.Int("port", 3174, "Port to listen on")
 
 func main() {
 	flag.Parse()
+	var logger *log.Logger
+	var logfile, logfileerr = os.OpenFile("borker.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if logfileerr != nil {
+		log.Println("Error opening observable.log file:", logfileerr)
+		logger = log.New(os.Stdout, "[🐶: stdout]", log.LstdFlags)
+	} else {
+		defer logfile.Close()
+		logger = log.New(logfile, "[🐶:]", log.LstdFlags)
+	}
+	var ChatroomPool = NewMessageObservable(logger)
+	go ChatroomPool.Borker()
 	app := fiber.New(fiber.Config{
 		Views: html.New("./views", ".html"),
 	})
@@ -34,6 +47,9 @@ func main() {
 	})
 
 	app.Use("/relay/chatroom", websocket.New(func(c *websocket.Conn) {
+		timeOfConnection := time.Now().String()
+		ChatroomPool.SubscriberChannel <- ObserverEntry{id: timeOfConnection, connection: c}
+		defer ChatroomPool.Unsubscribe(ObserverEntry{id: timeOfConnection, connection: c})
 
 		var (
 			messageType int
@@ -45,7 +61,7 @@ func main() {
 				log.Println("read:", err)
 				break
 			}
-			log.Printf("recv: %s on mt: %d", msg, messageType)
+			log.Printf("recv: %s on mt: %d, observerCount %v", msg, messageType, len(ChatroomPool.observers))
 
 			parsedJsonRequest, err := UnmarshaledJsonFromByteArray[struct {
 				Sender  string
@@ -56,21 +72,16 @@ func main() {
 				log.Println("Failed to parse request data", err)
 			}
 
-			log.Printf("Parsed Request: %v %v \n", parsedJsonRequest.Sender, parsedJsonRequest.Message)
-			parsedCompomnent, err := GetParsedMessageComponent(MesageDataType{
-				Sender:  parsedJsonRequest.Sender,
-				Content: parsedJsonRequest.Message,
-			})
-			if err != nil {
-				log.Println("Failed to parse message template", err)
-			}
-			if err = c.WriteMessage(messageType, parsedCompomnent); err != nil {
-				log.Println("write:", err)
-				break
-			}
-		}
-	}))
+			log.Printf("Parsed Request on chatroom2: %v %v \n", parsedJsonRequest.Sender, parsedJsonRequest.Message)
 
+			ChatroomPool.MessagesChannel <- MessageData{
+				sender:  parsedJsonRequest.Sender,
+				message: parsedJsonRequest.Message,
+			}
+
+		}
+
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("pages/index", fiber.Map{
 			"Title":  "Home: Le Chat Room",
@@ -79,4 +90,5 @@ func main() {
 	})
 
 	log.Fatal(app.Listen(fmt.Sprintf("%s:%d", *host, *port)))
+	ChatroomPool.Terminate()
 }
